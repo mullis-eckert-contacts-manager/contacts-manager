695fbd278a44c6624ea4c56c9f1421f75bde02ae
trevormullis12 <trevor.mullis12@gmail.com>
2020-06-22 16:16:05 -0500

    added some functionality to list and display contacts

--- src/Contact.java
+++ src/Contact.java
@@ -2,5 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Contact {
+    private String name;
+    private String number;
 
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public static List<Contact> stringsToContacts(List<String> contactText) {
+        String[] nameAndNumber;
+        List<Contact> contacts = new ArrayList<>();
+        for (String contact : contactText) {
+            nameAndNumber = contact.split(":");
+            contacts.add(new Contact(nameAndNumber[0], nameAndNumber[1]));
+        }
+        return contacts;
+    }
 }
